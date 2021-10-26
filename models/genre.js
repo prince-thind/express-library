222148b99984df576e496e9dda0d1b49eae1a0f4
Prince-Thind <princethind799@gmail.com>
2021-10-26 21:41:23 +0530

    add models

---
+++ models/genre.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Genre = new Schema({
+  name: { type: String, minLength: 3, maxLength: 100 },
+});
+
+Genre.virtual('url').get(function(){
+    return '/catalog/genre/'+this._id;
+})
