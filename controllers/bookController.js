39ceb65064e19a97eb403fc274f19a20f2c304a5
Prince-Thind <princethind799@gmail.com>
2021-10-27 18:45:35 +0530

    implement book list

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -36,7 +36,15 @@ exports.index = function (req, res) {
 
 // Display list of all books.
 exports.book_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book list');
+  Book.find({}, 'title author')
+    .sort({ title: 1 })
+    .populate('author')
+    .exec(function (err, list_books) {
+      if (err) {
+        return next(err);
+      }
+      res.render('book_list', { title: 'Book List', book_list: list_books });
+    });
 };
 
 // Display detail page for a specific book.
