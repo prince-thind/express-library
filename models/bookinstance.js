16e3e2fed102cf020a7165b54832675b62b8da53
Prince-Thind <princethind799@gmail.com>
2021-10-27 19:04:50 +0530

    udpate date formtting

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const {DateTime} = require('luxon');
 
 const BookInstanceSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
@@ -16,6 +17,10 @@ const BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual('url').get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
