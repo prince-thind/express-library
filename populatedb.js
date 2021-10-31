52b50a7ca78ebf5559d1bc27f03ec7697f10608d
Prince-Thind <princethind799@gmail.com>
2021-10-31 11:37:44 +0530

    add support for URI stored in .env file

--- populatedb.js
+++ populatedb.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+require('dotenv').config();
 
 console.log(
   'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true'
@@ -19,7 +20,7 @@ const Genre = require('./models/genre');
 const BookInstance = require('./models/bookinstance');
 
 const mongoose = require('mongoose');
-const mongoDB = userArgs[0];
+const mongoDB = process.env.MONGODB_URI||userArgs[0];
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
